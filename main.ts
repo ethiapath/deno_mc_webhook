9e5c7a3418fa3a45e6c8b00438e02b51d462afc3
Hargobind Atwal <atwalhargobind@gmail.com>
2021-08-17 20:05:53 -0700

    init commit

---
+++ main.ts
@@ -0,0 +1,33 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+
+const messages = [];
+const channel = new BroadcastChannel("chat");
+channel.onmessage = (event) => {
+  messages.push(event.data);
+};
+
+const router = new Router();
+router
+  .get("/", (context) => {
+	  console.log('tessst main route');
+    context.response.body = "Chat server!";
+  })
+  .get("/messages", (context) => {
+	  console.log('messeges route')
+    context.response.body = messages;
+  })
+  .post("/messages", async (context) => {
+    const message = await context.request.body().value;
+    messages.push(message);
+    channel.postMessage(message);
+    context.response.body = messages;
+  });
+
+const app = new Application();
+app.use(oakCors());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+addEventListener("fetch", app.fetchEventHandler());
+
